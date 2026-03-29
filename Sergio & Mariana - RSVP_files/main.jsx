f81329882824d6d7765395f37feb7c67971f343f
dammosg <damian.sevilla1@hotmail.com>
2026-03-29 15:42:52 -0600

    Updates

---
+++ Sergio & Mariana - RSVP_files/main.jsx
@@ -0,0 +1,16 @@
+import __vite__cjsImport0_react from "/s-m/node_modules/.vite/deps/react.js?v=0366d708"; const StrictMode = __vite__cjsImport0_react["StrictMode"];
+import __vite__cjsImport1_reactDom_client from "/s-m/node_modules/.vite/deps/react-dom_client.js?v=0366d708"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/s-m/src/App.jsx?t=1774820048264";
+var _jsxFileName = "/Users/damian.sevilla/Documents/sergio-mariana/src/main.jsx";
+import __vite__cjsImport3_react_jsxDevRuntime from "/s-m/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=0366d708"; const _jsxDEV = __vite__cjsImport3_react_jsxDevRuntime["jsxDEV"];
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ _jsxDEV(StrictMode, { children: /* @__PURE__ */ _jsxDEV(App, {}, void 0, false, {
+	fileName: _jsxFileName,
+	lineNumber: 7,
+	columnNumber: 5
+}, this) }, void 0, false, {
+	fileName: _jsxFileName,
+	lineNumber: 6,
+	columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJtYXBwaW5ncyI6IkFBQUEsU0FBUyxrQkFBa0I7QUFDM0IsU0FBUyxrQkFBa0I7QUFDM0IsT0FBTyxTQUFTOzs7QUFFaEIsV0FBVyxTQUFTLGVBQWUsT0FBTyxDQUFDLENBQUMsT0FDMUMsd0JBQUMsWUFBRCxZQUNFLHdCQUFDLEtBQUQsRUFBTzs7OztVQUNJOzs7O1NBQ2QiLCJuYW1lcyI6W10sInNvdXJjZXMiOlsibWFpbi5qc3giXSwidmVyc2lvbiI6Mywic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgU3RyaWN0TW9kZSB9IGZyb20gJ3JlYWN0J1xuaW1wb3J0IHsgY3JlYXRlUm9vdCB9IGZyb20gJ3JlYWN0LWRvbS9jbGllbnQnXG5pbXBvcnQgQXBwIGZyb20gJy4vQXBwLmpzeCdcblxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpKS5yZW5kZXIoXG4gIDxTdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9TdHJpY3RNb2RlPixcbilcbiJdfQ==
